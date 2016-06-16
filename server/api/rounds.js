d1b9b70070a709863127537e7f4d24323280a944
Jesse Ditson <jesse.ditson@gmail.com>
2016-06-15 23:25:13 -0700

    check in rounds before I remove

--- server/api/rounds.js
+++ server/api/rounds.js
@@ -6,21 +6,13 @@ var toJSONAPI = require('../lib/JSONAPI')('/api')
 router.get('/', (req, res, next) => {
   db('rounds')
     .select('*')
-    .then(rounds => {
-      return db('matches')
-        .select('*')
-        .whereIn('round_id', rounds.map(r => r.id))
+    .map(round => {
+      // TODO: we should only be creating links here, and letting ember assemble the relationships - but need to make matches do the same, etc if we do that.
+      return getMatchesByRound(round.id)
         .then(matches => {
-          var matchMap = matches.reduce((o, m) => {
-            var rid = m.round_id
-            if (!o[rid]) o[rid] = []
-            o[rid].push(m)
-            return o
-          }, {})
-          return rounds.map(r => {
-            r.relationships = { matches: matchMap[r.id] }
-            return r
-          })
+          console.log(matches)
+          round.relationships = { matches: matches }
+          return round
         })
     })
     .then(rounds => {
@@ -29,52 +21,63 @@ router.get('/', (req, res, next) => {
     .catch(next)
 })
 
-router.get('/:id/relationships/matches', (req, res, next) => {
-  var roundId = req.params.id
-  db('rounds')
+function getMatchesByRound(roundId) {
+  return db('matches')
     .select('*')
-    .where('id', roundId)
-    .then(round => {
-      return db('matches')
+    .where('round_id', roundId)
+    .then(matches => {
+      var bandIds = matches
+        .reduce((a, m) => {
+          a.push(m.band1_id)
+          a.push(m.band2_id)
+          return a
+        }, [])
+        .filter(b => !!b || b === 0)
+      return db('bands')
         .select('*')
-        .where('round_id', roundId)
-        .then(matches => {
-          var bandIds = matches
-            .reduce((a, m) => {
-              a.push(m.band1_id)
-              a.push(m.band2_id)
-              return a
-            }, [])
-            .filter(b => !!b || b === 0)
-          console.log(bandIds)
-          if (!bandIds.length) {
-            // if there are no bands, break early
-            round.relationships = { matches: matches }
-            return round
-          }
-          return db('bands')
-            .select('*')
-            .whereIn('id', bandIds)
-            .then(bands => {
-              var bandMap = bands.reduce((o, b) => {
-                o[b.id] = b
-                return o
-              }, {})
-              round.relationships = {
-                matches: matches.map(m => {
-                  m.relationships = {
-                    band1: bandMap[m.band1_id],
-                    band2: bandMap[m.band2_id]
-                  }
-                  return m
-                })
-              }
-              return round
-            })
+        .whereIn('id', bandIds)
+        .then(bands => {
+          var bandMap = bands.reduce((o, b) => {
+            o[b.id] = { type: 'bands', value: b }
+            return o
+          }, {})
+          return matches.map(m => {
+            m.relationships = {
+              band1: bandMap[m.band1_id],
+              band2: bandMap[m.band2_id]
+            }
+            return m
+          })
         })
     })
+}
+
+function getRoundById(roundId) {
+  return db('rounds')
+    .select('*')
+    .where('id', roundId)
+    .first()
+    .then(round => {
+      return [round, getMatchesByRound(round.id)]
+    })
+    .spread((round, matches) => {
+      round.relationships = { matches: matches }
+      return round
+    })
+}
+
+router.get('/:id', (req, res, next) => {
+  getRoundById(req.params.id)
     .then(round => {
       res.json(toJSONAPI('rounds', round))
+    })
+    .catch(next)
+})
+
+router.get('/:id/matches', (req, res, next) => {
+  getMatchesByRound(req.params.id)
+    .then(matches => {
+      res.json(toJSONAPI('matches', matches))
     })
     .catch(next)
 })
