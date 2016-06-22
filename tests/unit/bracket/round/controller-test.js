5faf00f7ea1d095b861f7a7fa45d87bbf5ffb432
Jesse Ditson <jesse.ditson@gmail.com>
2016-06-21 17:17:51 -0700

    add controller test

---
+++ tests/unit/bracket/round/controller-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:bracket/round', 'Unit | Controller | bracket/round', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
