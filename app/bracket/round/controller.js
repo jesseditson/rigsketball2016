a85221e3180c8b62af9f9296b0ecea106fb78997
Jesse Ditson <jesse.ditson@gmail.com>
2016-06-23 10:29:11 -0700

    sort by time

--- app/bracket/round/controller.js
+++ app/bracket/round/controller.js
@@ -13,7 +13,7 @@ export default Ember.Controller.extend({
     var matchInfo = fullMatchInfo(this.get('model.rounds'))
     var matches = this.get('model.rounds').find(r => {
       return r.get('id') === this.get('model.round.id')
-    }).get('matches').sortBy('index')
+    }).get('matches').sortBy('time')
     return matches.map((match) => {
       var idx = match.get('index')
       // console.log(idx, JSON.parse(JSON.stringify(matchInfo[idx])))
